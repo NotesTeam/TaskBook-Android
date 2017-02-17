920b44e4c250e39c9103e80b502fc5c6b39e0150
wzieba <wojciech.zieba@protonmail.com>
2017-02-17 21:15:49 +0100

    WIP: Reimplementation of Drawer Navigation

--- app/src/main/java/com/twago/TaskBook/NoteMain/NoteMainActivity.java
+++ app/src/main/java/com/twago/TaskBook/NoteMain/NoteMainActivity.java
@@ -1,17 +1,16 @@
 package com.twago.TaskBook.NoteMain;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-import com.mikepenz.materialdrawer.Drawer;
-import com.mikepenz.materialdrawer.DrawerBuilder;
-import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
-import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 import com.twago.TaskBook.Module.Constants;
 import com.twago.TaskBook.NoteList.ListFragment;
 import com.twago.TaskBook.R;
@@ -25,7 +24,12 @@ public class NoteMainActivity extends AppCompatActivity {
     private static final String TAG = NoteMainActivity.class.getSimpleName();
     private ListFragment noteListFragment;
     private MainContract.UserActionListener mainUserActionListener;
-    private Drawer drawer;
+    ActionBarDrawerToggle toggle;
+
+    DrawerLayout drawerLayout;
+    View drawerView;
+    View drawerContent;
+    View mainContent;
 
     @BindView(R.id.button_create_note)
     FloatingActionButton createNoteButton;
@@ -45,8 +49,45 @@ public class NoteMainActivity extends AppCompatActivity {
         noteListFragment = ListFragment.newInstance();
         getFragmentManager()
                 .beginTransaction()
-                .add(R.id.fragment_layout, noteListFragment)
+                .add(R.id.main_view, noteListFragment)
                 .commit();
+
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerView = findViewById(R.id.drawer_view);
+        drawerContent = findViewById(R.id.drawer_content);
+        mainContent = findViewById(R.id.main_view);
+
+        drawerLayout.setScrimColor(Color.TRANSPARENT);
+
+
+        toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        drawerLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                drawerContent.setX(drawerView.getWidth() * (1 - slideOffset));
+                mainContent.setX(drawerView.getWidth() * slideOffset);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+
+            }
+        });
+
+        toggle.syncState();
+
     }
 
     @Override
@@ -76,60 +117,51 @@ public class NoteMainActivity extends AppCompatActivity {
     }
 
     @OnClick(R.id.button_create_note)
-    public void onCreateNote(){
+    public void onCreateNote() {
         noteListFragment.getPresenter().openNewEditor(Constants.NEW_NOTE_ID);
     }
 
     private void buildDrawer() {
-        drawer = new DrawerBuilder()
-                .withActivity(this)
-                .withToolbar(toolbar)
-                .addDrawerItems(
-                        buildActiveTasksDrawerItem(),
-                        buildArchiveDrawerItem()
-                )
-                .withSliderBackgroundColorRes(R.color.dark_blue)
-                .withDrawerWidthDp(60)
-                .build();
+
     }
 
-    private PrimaryDrawerItem buildActiveTasksDrawerItem() {
-        return new PrimaryDrawerItem()
-                .withIcon(R.drawable.ic_date_range_white_36dp)
-                .withName(R.string.tasks)
-                .withSelectedColorRes(R.color.granate)
-                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
-                    @Override
-                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
-                        mainUserActionListener.setupSubscriberActiveTasks();
-                        setTitle(R.string.tasks);
-                        createNoteButton.setVisibility(View.VISIBLE);
-                        return false;
-                    }
-                });
-    }
-
-    private PrimaryDrawerItem buildArchiveDrawerItem() {
-        return new PrimaryDrawerItem()
-                .withIcon(R.drawable.ic_archive_white_36dp)
-                .withName(R.string.archive)
-                .withSelectedColorRes(R.color.granate)
-                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
-                    @Override
-                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
-                        mainUserActionListener.setupSubscriberArchive();
-                        setTitle(R.string.archive);
-                        createNoteButton.setVisibility(View.INVISIBLE);
-                        return false;
-                    }
-                });
-    }
+//    private PrimaryDrawerItem buildActiveTasksDrawerItem() {
+//        return new PrimaryDrawerItem()
+//                .withIcon(R.drawable.ic_date_range_white_36dp)
+//                .withName(R.string.tasks)
+//                .withSelectedColorRes(R.color.granate)
+//                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+//                    @Override
+//                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+//                        mainUserActionListener.setupSubscriberActiveTasks();
+//                        setTitle(R.string.tasks);
+//                        createNoteButton.setVisibility(View.VISIBLE);
+//                        return false;
+//                    }
+//                });
+//    }
+//
+//    private PrimaryDrawerItem buildArchiveDrawerItem() {
+//        return new PrimaryDrawerItem()
+//                .withIcon(R.drawable.ic_archive_white_36dp)
+//                .withName(R.string.archive)
+//                .withSelectedColorRes(R.color.granate)
+//                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+//                    @Override
+//                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+//                        mainUserActionListener.setupSubscriberArchive();
+//                        setTitle(R.string.archive);
+//                        createNoteButton.setVisibility(View.INVISIBLE);
+//                        return false;
+//                    }
+//                });
+//    }
 
     public void toggleDrawer() {
-        if (drawer.isDrawerOpen())
-            drawer.closeDrawer();
-        else
-            drawer.openDrawer();
+//        if (drawer.isDrawerOpen())
+//            drawer.closeDrawer();
+//        else
+//            drawer.openDrawer();
     }
 
     private void buildMenuButton() {
